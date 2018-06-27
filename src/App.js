aadbd5d3ee2844dabd1881c2d1ea3f40b5172b30
Gia1987 <giacomo.n27@gmail.com>
2018-06-27 21:56:13 +0100

    Refactored code API city and country

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Titles from './component/Titles';
 import Form  from './component/Form.js';
 import Weather  from './component/Weather.js';
 
-const API_KEY = 'a3d9eb01d4de82b9b8d0849ef604dbed';
+const API_KEY = "a3d9eb01d4de82b9b8d0849ef604dbed";
 
 class App extends React.Component {
-  getWheater = async (e) => {
+  getWeather = async(e) => {
     // async await
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    console.log(data);
+
   }
   render() {
     return (
